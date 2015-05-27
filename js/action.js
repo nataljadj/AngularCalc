781149fb08f46cd42e0fc021bdd43bd2b30b47d5
unknown <ndiedyk@KOTROL.softservecom.com>
2015-05-27 14:47:29 +0300

    add congig files

--- js/action.js
+++ js/action.js
@@ -1,44 +1,49 @@
+(function(){
+	calcModel = {
+		number: 0,
+		operation: '',
+		result: 0,
+		output: '',
 
-calcModel = {
-	number: 0,
-	operation: '',
-	result: 0,
-	output: '',
+		reset: function(){
+			this.number = 0;
+			this.operation = '';
+			this.result = 0;
+			this.output = '';
+		},
 
-	reset: function(){
-		this.number = 0;
-		this.operation = '';
-		this.result = 0;
-		this.output = '';
-	},
+		chooseOperation: function(setoperation){
+			this.operation = setoperation;
+			if (calcModel.number === '0') {
+				this.calcModel.output += 0;
+			}
+			this.output += " " + this.operation + " ";
+			this.getResult();
 
-	chooseOperation: function(setoperation){
-		this.operation = setoperation;
-		if (calcModel.number === '0') {
-			this.calcModel.output += 0;
-		}
-		this.output += " " + this.operation + " ";
-		this.getResult();
+		},
+	};
 
-	},
-};
+	var appCalc = angular.module('appCalc',['ngRoute']);
+	appCalc.config(function($routeProvider){
+		$routeProvider
 
+			.when('/',{
+				templateUrl : 'pages/home.html',
+				controller  : 'myCtrl'
+			})
+	});
+	appCalc.controller('myCtrl',['$scope', function($scope){
+		$scope.calculator = calcModel;
+		$scope.buttonClick = function(value){
+			if(value === 'Ce'){
+				calcModel.output = '' ;
+			}
+			if (value === '=') {
 
-var appCalc = angular.module('appCalc',[]);
-appCalc.controller('myCtrl',['$scope', function($scope){
-	$scope.calculator = calcModel;
-	$scope.buttonClick = function(value){
-		if(value === 'Ce'){
-			calcModel.output = '' ;
-		}
-		if (value === '=') {
-
-		};
-		else{
-			calcModel.number += value;
-	 		calcModel.output += value;
-		}
-	}};
-	$scope.getResult
-	]
-);
+			}
+			else{
+				calcModel.number += value;
+		 		calcModel.output += value;
+			}
+		}}]);
+})();
