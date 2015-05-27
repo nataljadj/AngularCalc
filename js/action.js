c9e3f91e80dd7411486ddb13a4c3e3d456b0b2c2
unknown <ndiedyk@KOTROL.softservecom.com>
2015-05-27 10:36:16 +0300

    the original version but don't work at all

---
+++ js/action.js
@@ -0,0 +1,44 @@
+
+calcModel = {
+	number: 0,
+	operation: '',
+	result: 0,
+	output: '',
+
+	reset: function(){
+		this.number = 0;
+		this.operation = '';
+		this.result = 0;
+		this.output = '';
+	},
+
+	chooseOperation: function(setoperation){
+		this.operation = setoperation;
+		if (calcModel.number === '0') {
+			this.calcModel.output += 0;
+		}
+		this.output += " " + this.operation + " ";
+		this.getResult();
+
+	},
+};
+
+
+var appCalc = angular.module('appCalc',[]);
+appCalc.controller('myCtrl',['$scope', function($scope){
+	$scope.calculator = calcModel;
+	$scope.buttonClick = function(value){
+		if(value === 'Ce'){
+			calcModel.output = '' ;
+		}
+		if (value === '=') {
+
+		};
+		else{
+			calcModel.number += value;
+	 		calcModel.output += value;
+		}
+	}};
+	$scope.getResult
+	]
+);
